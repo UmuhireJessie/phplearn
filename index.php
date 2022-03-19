621e0295bf84f2f1eb007747cdf8c72cd064b52a
UmuhireJessie <j.umuhire@alustudent.com>
2022-03-19 12:41:51 +0200

    first project php embedded in html

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+// echo 'Hello, Jessie';
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>my first PHP file</title>
+</head>
+<body>
+    <h1><?php echo 'Hello, Jessie'; ?></h1>
+</body>
+</html>
