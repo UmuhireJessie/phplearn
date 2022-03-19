b7cfa7b7fbcd2c9d0d4ef09d2581273ecdc5218d
UmuhireJessie <j.umuhire@alustudent.com>
2022-03-19 13:06:46 +0200

    variables and constants

--- index.php
+++ index.php
@@ -1,15 +1,28 @@
 <?php
 
-// echo 'Hello, Jessie';
+define('COUNTRY', 'Rwanda');
+
+$name = 'Jessie';
+$age = 20;
+
+// Overiding the variable name
+$name = "Umuhire"
+
+// Error
+define('COUNTRY', 'USA');
 
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-    <title>my first PHP file</title>
+    <title>PHP Learn</title>
 </head>
 <body>
     <h1><?php echo 'Hello, Jessie'; ?></h1>
+
+    <div><?php echo COUNTRY; ?></div>
+    <div><?php echo $name ?></div>
+    <div><?php echo $age ?></div>
 </body>
 </html>
