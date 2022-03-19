dc243a60b9648e60dfa4d870584aeb97f02a20f1
UmuhireJessie <j.umuhire@alustudent.com>
2022-03-19 17:07:58 +0200

    booleans in php

--- index.php
+++ index.php
@@ -1,37 +1,25 @@
 <?php
 
-$ninjas = ['shaun', 'ryu', 'yoshi'];
-    // For loop
-// for($i = 0; $i < count($ninjas); $i++) {
-//     echo $ninjas[$i] . '<br/>';
-// };
+    // comparisons boolean (true or false)
+// echo true; "1"
+// echo false; ""
 
-    // Foreach loop
-// foreach($ninjas as $ninja) {
-//     echo $ninja .'<br />';
-// };
+    // numbers
+// echo 5 < 10;
+// echo 5> 10;
+// echo 5 == 10;
 
-$products = [
-    ['name' => 'shiny star', 'price' => 20],
-    ['name' => 'green shell', 'price' => 10],
-    ['name' => 'red shell', 'price' => 15],
-    ['name' => 'gold coin', 'price' => 5],
-    ['name' => 'lightning bolt', 'price' => 40],
-    ['name' => 'banan akin', 'price' => 2]
-];
+    //strings
+// echo 'shaun' < 'yoshi';
+// echo 'shaun' > 'yoshi';
+// echo 'shaun' > 'Shaun';    "1"
 
-// foreach($products as $product) {
-//     echo $product['name'] . ' - ' . $product['price'];
-//     echo '<br />';
-// }
+    // loose vs strict equal comparison
+// echo 5 == '5'; 
+// echo 5 === '5';
 
-    // Aw while loop
-// $i = 0;
-// while ($i < count($products)) {
-//     echo $products[$i]['name'];
-//     echo '<br />';
-//     $i++;
-// }
+// echo true == '1';
+// echo false == "";
 
 ?>
 
@@ -41,14 +29,6 @@ $products = [
     <title>PHP Learn</title>
 </head>
 <body>
-    <h1>Products</h1>
-    <ul>
-        <?php foreach ($products as $product) { ?>
-        
-            <h3><?php echo $product['name'];?></h3>
-            <p> $ <?php echo $product['price']; ?></p>
-
-        <?php } ?>
-    </ul>
+    
 </body>
 </html>
