f8f64f4a1ae04ed9ef11eece0eef701641bd9e8c
UmuhireJessie <j.umuhire@alustudent.com>
2022-03-19 17:45:10 +0200

    continue and break statements

--- index.php
+++ index.php
@@ -1,15 +1,6 @@
 <?php
 
-    // conditional statements
-// $price = 20;
-
-// if ($price < 10) {
-//     echo 'the condition is met';
-// } else if ($price < 30) {
-//     echo 'elseif condition met';
-// }else {
-//     echo 'condition not met';
-// }
+    // break and continue
 
 $products = [
     ['name' => 'shiny star', 'price' => 20],
@@ -21,13 +12,13 @@ $products = [
 ];
 
 foreach ($products as $product) {
-    // if($product['price'] < 15 && $product['price'] > 2) {
-    //     echo $product['name'] . "<br />";
-    // }
-
-    // if($product['price'] > 20 || $product['price'] < 10) {
-    //     echo $product['name'] . "<br />";
-    // }
+    if ($product['name'] === 'lightning bolt') {
+        break;
+    }
+    if ($product['price'] > 15){
+        continue;
+    }
+    echo $product['name'] . "<br />";
 }
 
 ?>
@@ -39,15 +30,7 @@ foreach ($products as $product) {
 </head>
 <body>
     
-    <div>
-        <ul>
-            <?php foreach($products as $product) { ?>
-                <?php if($product['price'] > 15) {?>
-                    <li><?php echo $product['name'] ?></li>
-                <?php } ?>
-            <?php } ?>
-        </ul>
-    </div>
+    
 
 </body>
 </html>
