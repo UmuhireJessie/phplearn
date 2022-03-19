9623f0208261d7b82c42f3bdddb1f6be7085140c
UmuhireJessie <j.umuhire@alustudent.com>
2022-03-19 18:50:34 +0200

    require and include files

---
+++ content.php
@@ -0,0 +1,3 @@
+<div>
+    <h1><?php echo 'some content, wow' ?></h1>
+</div>
--- index.php
+++ index.php
@@ -1,35 +1,11 @@
 <?php
 
-    // local var 
-function myFunc() {
-    $price = 10;
-    echo $price;
-}
+// include ('ninjas.php');
+require 'ninjas.php';
 
-// myFunc();
-// echo $price; // Error
+// require 'ninljas.php';  // error: fatal <> error and continue on the include
 
-    // global var 
-
-$name = 'mario';
-// function sayHello() {
-//     global $name;
-//     $name = 'yoshi';
-//     echo "hello $name";
-// }
-// sayHello();
-// echo $name; //changed too 
-
-function sayBye(&$name) { // add & references the one in global
-    $name = 'wario';
-    echo "bye $name";
-}
-sayBye($name);
-echo $name;  // not changed since the function was not reffering to the name
-
-
-
-
+echo 'end of php';
 
 ?>
 
@@ -40,7 +16,9 @@ echo $name;  // not changed since the function was not reffering to the name
 </head>
 <body>
     
-    
+    <?php include 'content.php'; ?>
+    <?php include 'content.php'; ?>
+    <?php include 'content.php'; ?>
 
 </body>
 </html>
