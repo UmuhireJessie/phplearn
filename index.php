0977b5cf3fbff0a440ac70ce51c9912cab9ded79
UmuhireJessie <j.umuhire@alustudent.com>
2022-03-20 14:27:54 +0200

    connecting and retrieving data from the database as an array

--- index.php
+++ index.php
@@ -8,6 +8,23 @@
         echo 'Connection error: ' . mysqli_connect_error();
     }
 
+    // write query from all pizzas
+    $sql = 'SELECT title, ingredients, id FROM pizzas';
+
+    //make query and get result
+    $result = mysqli_query($conn, $sql);
+
+    // fetch the resulting rows as an array
+    $pizzas = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+    // free result from memory
+    mysqli_free_result($result);
+
+    //close the connection
+    mysqli_close($conn);
+
+    print_r($pizzas);
+
 ?>
 
 <!DOCTYPE html>
