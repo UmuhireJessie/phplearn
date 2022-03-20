6c1c540f547c3e89f59eda7640d3ab69fa2ca041
UmuhireJessie <j.umuhire@alustudent.com>
2022-03-20 11:01:19 +0200

    project header, footer and add a pizza in the cat

--- content.php
+++
@@ -1,3 +0,0 @@
-<div>
-    <h1><?php echo 'some content, wow' ?></h1>
-</div>
--- index.php
+++ index.php
@@ -1,24 +1,15 @@
 <?php
 
-// include ('ninjas.php');
-require 'ninjas.php';
 
-// require 'ninljas.php';  // error: fatal <> error and continue on the include
-
-echo 'end of php';
 
 ?>
 
 <!DOCTYPE html>
 <html>
-<head>
-    <title>PHP Learn</title>
-</head>
-<body>
-    
-    <?php include 'content.php'; ?>
-    <?php include 'content.php'; ?>
-    <?php include 'content.php'; ?>
 
-</body>
+<?php include ('template/header.php'); ?>
+    
+<?php include ('template/footer.php'); ?>
+
+
 </html>
