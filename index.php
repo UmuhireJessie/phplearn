e0a3833fadf68e96eb8e298222efa24d3d314c7b
UmuhireJessie <j.umuhire@alustudent.com>
2022-03-20 14:44:53 +0200

    creating the cards on html from the database

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     }
 
     // write query from all pizzas
-    $sql = 'SELECT title, ingredients, id FROM pizzas';
+    $sql = 'SELECT title, ingredients, id FROM pizzas ORDER BY created_at';
 
     //make query and get result
     $result = mysqli_query($conn, $sql);
@@ -23,14 +23,35 @@
     //close the connection
     mysqli_close($conn);
 
-    print_r($pizzas);
-
 ?>
 
 <!DOCTYPE html>
 <html>
 
 <?php include ('template/header.php'); ?>
+
+<h4 class="center grey-text">Pizzas</h4>
+<div class="container">
+    <div class="row">
+
+        <?php foreach ($pizzas as $pizza) { ?>
+            
+            <div class="col s6 md3 ">
+                <div class="card z-depth-0">
+                    <div class="card-content center">
+                        <h6><?php echo htmlspecialchars($pizza['title']); ?></h6>
+                        <div><?php echo htmlspecialchars($pizza['ingredients']) ?></div>
+                    </div>
+                    <div class="card-action right-align">
+                        <a href="#" class="brand-text">more info</a>
+                    </div>
+                </div>
+            </div>
+
+        <?php } ?>
+
+    </div>
+</div>
     
 <?php include ('template/footer.php'); ?>
 
