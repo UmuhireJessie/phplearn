807d2f170dda29b257319ab5ecb4074aae65cc7e
UmuhireJessie <j.umuhire@alustudent.com>
2022-03-20 20:02:09 +0200

    class with private instances

--- sandbox.php
+++ sandbox.php
@@ -4,8 +4,8 @@
 
     class User {
 
-        public $email;
-        public $name;
+        private $email;
+        private $name;
 
         public function __construct($name, $email) {
             // $this -> email = 'mario@gmail.com';
@@ -18,18 +18,27 @@
             // echo 'the user login in';
             echo $this->name . 'logged in';
         }
+
+        public function getName() {
+            return $this -> name;
+        }
+
+        public function setName($name) {
+            if (is_string($name) && strlen($name) > 1) {
+                $this -> name = $name;
+                return "name has been changed to $name";
+            } else {
+                return 'not a valid name';
+            }
+        }
+
     }
 
-    // $userOne = new User();
-  
-    // $userOne -> login();
-    // echo $userOne -> email;
-
     $userTwo = new User('yoshi', 'yoshi@gmail.com');
-    // echo $userTwo -> name;
-    // echo $userTwo -> email;
+    // echo $userTwo -> getName();
+    // echo $userTwo -> setName(40);
+    echo $userTwo -> setName('shaun');
+    echo $userTwo -> getName();
     
-    $userTwo -> login();
-
 
 ?>
