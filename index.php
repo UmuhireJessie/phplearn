531eb4cf1de56a0c0b968f1251f8d66b4bc5c1d0
UmuhireJessie <j.umuhire@alustudent.com>
2022-03-20 14:14:16 +0200

    connecting to the database

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <?php
 
+    // connect to databse 
+    $conn = mysqli_connect('localhost', 'root', 'jessie@17', 'ninja_pizza');
 
+    // check the connection
+    if (!$conn) {
+        echo 'Connection error: ' . mysqli_connect_error();
+    }
 
 ?>
 
