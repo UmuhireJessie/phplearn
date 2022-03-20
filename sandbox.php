373511e075452b845c4d29b65de5d27122c6cbb0
UmuhireJessie <j.umuhire@alustudent.com>
2022-03-20 17:20:17 +0200

    ternary operators, (shortning if)

---
+++ sandbox.php
@@ -0,0 +1,28 @@
+<?php
+
+    // ternary operators
+
+    $score = 20;
+
+    // if ($score >40) {
+    //     echo 'high score';
+    // } else {
+    //     echo 'low score' ;
+    // }
+
+    // echo $score > 40 ? 'high score!' : 'low score :(';
+    
+
+?>
+
+<!DOCTYPE html>
+<html">
+<head>
+    <title>Document</title>
+</head>
+<body>
+
+<p> <?php echo $score > 40 ? 'high score!' : 'low score :(';  ?> <p>
+    
+</body>
+</html>
